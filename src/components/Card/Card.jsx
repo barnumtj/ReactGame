a880d5555bf74a0536378d19767bf195f3060c2f
barnumtj <tanner.barnum@icloud.com>
2018-09-26 17:44:36 -0500

    updated css

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -15,7 +15,7 @@ class FriendCard extends React.Component {
     render() {
     return (
     <div className="cardContainer">  
-            <img className='card'  onClick={() => this.props.handleClick(this.props.id)}  id={this.props.id} style={styles.card} alt={this.props.name} src={this.props.image}/>       
+            <img className='card updateCSS'  onClick={() => this.props.handleClick(this.props.id)}  id={this.props.id} style={styles.card} alt={this.props.name} src={this.props.image}/>       
     </div>
     )
 }
